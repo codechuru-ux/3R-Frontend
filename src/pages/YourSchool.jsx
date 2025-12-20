e5c577b0cd98e90324ac051066bfbf319d028ab2
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-20 14:14:40 +0530

    Update YourSchool.jsx

--- src/pages/YourSchool.jsx
+++ src/pages/YourSchool.jsx
@@ -5,6 +5,7 @@ import { AllProductsData } from "../context/AllProducts";
 import SchoolLoginForm from "../components/form/SchoolLoginForm";
 import { getSchoolProducts } from "../components/form/api"; 
 import Cards from "../components/Cards";
+
 const YourSchool = ({ auth, showAlert }) => {
   const [schoolDataList2, setSchoolDataList2] = useState([]);
   const schoolDataList = useContext(AllProductsData);
@@ -31,7 +32,7 @@ const YourSchool = ({ auth, showAlert }) => {
             <div className="flex justify-around min-w-full">
               <a href="/uploadProducts" className="bg-white cursor-pointer text-green-800 px-15 xl:mt-27 py-2 mt-7 border-2 shadow-xl border-white-800  inline-flex gap-2 font-semibold text-4xl font-serif items-center transition-all duration-700 rounded-[9px] group hover:text-white hover:bg-green-800">Upload Products</a>
             </div>
-            <div className="flex flex-wrap justify-center">
+            <div className="flex 2xl:gap-14 flex-wrap justify-around">
               {schoolDataList2.map((schoolData, index) => <Cards key={schoolData._id || index} title={schoolData.title} _id={schoolData._id} availability={schoolData.availability ? "Available" : "Unavailable"} thumbnail={`https://rrr-backend-0wj5.onrender.com/${schoolData.thumbnail}`} />)}
             </div>
           </div>
