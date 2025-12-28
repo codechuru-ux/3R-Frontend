d6ebeef4acf6803e228cb6a978ef71bd25607923
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-28 19:52:57 +0530

    Update AdminProfile.jsx

--- src/components/AdminProfile.jsx
+++ src/components/AdminProfile.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+const Photo = "/images/Super admin profile photo.png"
+
 const AdminProfile = ({ auth }) => (
     <div className="bg-white shadow-lg rounded-lg p-6 w-full">
-        <div className="w-32 h-32 rounded-full mx-auto mb-4 bg-gray-200 flex items-center justify-center border-4 border-green-500">
+        <div className="w-32 h-32 rounded-full mx-auto my-14 bg-gray-200 flex items-center justify-center border-4 border-green-500">
+          <img src={Photo} alt="" className="min-w-[200px] h-fit object-cover border-6 border-green-500 rounded-full"/>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
         </svg>
