d6f4c3185b5e93a2412288d3fb1f9b884a6a1448
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-29 11:57:53 +0530

    Update Inspiration.jsx

--- src/pages/Inspiration.jsx
+++ src/pages/Inspiration.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import InspirationCard from '../components/InspirationCard';
 import Slidebar from "../components/Slidebar";
 import Text from '../components/Text';
-const photo1 = "/images/pictac1.jpg"
+const photo1 = "/images/photo1.png"
 const photo2 = "/images/pictac2.jpg"
 const photo3 = "/images/pictac3.jpg"
 const photo4 = "/images/pictac4.png"
@@ -16,7 +16,7 @@ const Inspiration = () => {
        <h1 className='font-bold text-red-800 text-5xl  mt-18 font-serif'>- - Our Inspiration - -</h1>
      </div>
     <div className='flex flex-col justify-around'>
-    <InspirationCard  name='ABHISHEK SURANA ( I.A.S. )' image={photo1} post='District Collector and District Magistrate (DM),Churu'/>
+    <InspirationCard  name='ABHISHEK SURANA' image={photo1} post='District Collector and District Magistrate (DM),Churu'/>
     <InspirationCard  name='Dr.Santosh Kumar Maharshi' image={photo3} post='Chief District Education Officer(CDEO),Churu'/>
     
     </div>
