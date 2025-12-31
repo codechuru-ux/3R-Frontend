e0b98554d0a0b27f29c605b3aaef71a2675e420f
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-31 21:45:59 +0530

    Add files via upload

---
+++ src/pages/UserManagement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Slidebar from "../components/Slidebar";
+
+
+export default function UserManagement(){
+  return(
+    <>
+    <Slidebar />
+    <div className="min-h-screen w-full bg-[#f0f8ef]">
+
+    </div>
+    </>
+  );
+};
